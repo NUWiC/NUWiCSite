09fa4b72cfa946a6cbffa14785025fa12114d172
lizzb <lizz@lizz-natalia.local>
2014-04-01 14:39:49 -0500

    re-orged SO MUCH SHIT, in the process of merging
    taking blame for lack of communication since we were working in diff
    repos oops.

--- qinspired/other/customers_files/customer-scripts-v.2.js
+++
@@ -1,8 +0,0 @@
-
-jQuery(function($) {
-        $("#Overlay").on('click', function(){
-        $("#IframeContainer, #Overlay").hide();
-        $("#IframeContainer").find('iframe').attr('src', '');
-    });
-    
- });
